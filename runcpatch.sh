2dadade088ee169778a893f4c2fe8dcdb7b12928
zbjaaa1 <168157190+zbjaaa1@users.noreply.github.com>
2024-04-29 09:32:10 +0800

    Update runcpatch.sh

--- runcpatch.sh
+++ runcpatch.sh
@@ -7,5 +7,5 @@ row2=$(sed -n -e '/return 0/=' test2.txt)
 row3=$(echo `expr $row + $row2 - 1`)
 sed -i "$row3 i\        }" $RUNC_ADD_FILE
 sed -i "$row3 i\                kernfs_create_link(cgrp->kn, name, kn);" $RUNC_ADD_FILE
-sed -i "$row3 i\                snprintf(name, CGROUP_FILE_NAME_MAX, \"%s.%s\", cft->ss->name, cft->name);" $RUNC_ADD_FILE
-sed -i "$row3 i\        if (cft->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) && !(cft->flags & CFTYPE_NO_PREFIX)) {" $RUNC_ADD_FILE
+sed -i "$row3 i\                snprintf(name, CGROUP_FILE_NAME_MAX, \"%s.%s\", cfts->ss->name, cfts->name);" $RUNC_ADD_FILE
+sed -i "$row3 i\        if (cfts->ss && (cgrp->root->flags & CGRP_ROOT_NOPREFIX) && !(cfts->flags & CFTYPE_NO_PREFIX)) {" $RUNC_ADD_FILE
